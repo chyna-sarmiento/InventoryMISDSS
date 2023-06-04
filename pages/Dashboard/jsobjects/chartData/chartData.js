11f7abfc244187da1762854b5ba1dc72d71fcfb0
chyna-sarmiento <chyna-sarmiento>
2023-06-04 15:02:33 +0000

    chore (dashboard): use correct data

--- pages/Dashboard/jsobjects/chartData/chartData.js
+++ pages/Dashboard/jsobjects/chartData/chartData.js
@@ -58,9 +58,9 @@ export default {
 		return JSON.stringify(outputDataSource);
 	},
 	CustomerDemand() {
-		const barData = ShowProductList.data
-		.filter(p => p.stockCount < 20)
-		.map(p => ({ label: p.displayName, value: p.stockCount }));
+		const barData = forecastOutgoingStocksData.data
+		.filter(p => p.outgoingDemandVolume >= 10)
+		.map(p => ({ label: p.productName, value: p.outgoingDemandVolume }));
 
 		const maxBarValue = Math.max(...barData.map(data => data.y)) + 1;
 
@@ -68,9 +68,9 @@ export default {
 			type: "mscombi2d",
 			dataSource: {
 				chart: {
-					caption: "Inventory Stock",
-					xaxisname: "Low Items",
-					yaxisname: "Number of Items",
+					caption: "Customer Demand",
+					xaxisname: "Popular Items",
+					yaxisname: "Volume of Demand",
 					yaxismaxvalue: maxBarValue,
 					theme: "fusion",
 					baseFont: "Montserrat",
@@ -87,28 +87,6 @@ export default {
 					seriesname: "Current",
 					data: barData,
 					color: "#64748b"
-				}],
-				trendlines: [{
-					line: [{
-						startvalue: "10",
-						endvalue: "",
-						color: "#fde047",
-						displayvalue: "Warning",
-						valueonright: "1",
-						thickness: "2",
-						showontop: "1",
-						alpha: "80"
-					},
-								 {
-									 startvalue: "5",
-									 endvalue: "",
-									 color: "#ef4444",
-									 displayvalue: "Restock ASAP",
-									 valueonright: "1",
-									 thickness: "2",
-									 showontop: "1",
-									 alpha: "80"
-								 }]
 				}]
 			}
 		};
