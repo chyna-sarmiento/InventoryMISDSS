027d930db665f9af398469e3a861e8fe0d705a71
chyna-sarmiento <chyna-sarmiento>
2023-06-06 08:54:45 +0000

    chore (history): replaced ShowOutgoingStocks refreshOnAdd to ShowOutgoingList

--- pages/Sales History/jsobjects/apiOutgoingFunctions/apiOutgoingFunctions.js
+++ pages/Sales History/jsobjects/apiOutgoingFunctions/apiOutgoingFunctions.js
@@ -1,6 +1,6 @@
 export default {
 	refreshOnAdd() {
-		{{ ShowOutgoingStocks.run(() => {}, () => {}) }}
+		{{ ShowOutgoingList.run(() => {}, () => {}) }}
 	},
 	addOutgoingEntries: [],
 	createAddOutgoingEntriesStore() {
