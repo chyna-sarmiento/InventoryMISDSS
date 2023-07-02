974936ffa9dc3ba2079a0932da6a0dde925e470b
chyna-sarmiento <chyna-sarmiento>
2023-07-02 19:43:19 +0000

    save point: pre-merge from revisions branch

--- pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
+++ pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
@@ -1,11 +1,18 @@
 export default {
 	async executeQueriesInOrder() {
 		try {
-			await getProductForecast.run();
-			// await GetLatestDate.run();
-			// await GetEarliestDate.run();
-			// await dataDemandCurrent.run();
-			// await filteredDemandForecast.run();
+			// Execute the first query
+			await GetLatestDate.run();
+
+			// Execute the second query after the first one
+			await GetEarliestDate.run();
+			
+			// Execute the third query after the second one
+			await dataDemandCurrent.run();
+			
+			await filteredDemandForecast.run();
+
+			// Continue with other actions or logic
 			console.log("All queries executed successfully");
 		} catch (error) {
 			console.error("Error executing queries:", error);
