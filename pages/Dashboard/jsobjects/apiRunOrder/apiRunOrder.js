5ba2a79cb0e147e281710fe057b9f6396979f471
chyna-sarmiento <chyna-sarmiento>
2023-07-03 08:35:55 +0000

    refactor (library): reinstall papa library

--- pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
+++ pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
@@ -1,18 +1,11 @@
 export default {
 	async executeQueriesInOrder() {
 		try {
-			// Execute the first query
-			await GetLatestDate.run();
-
-			// Execute the second query after the first one
-			await GetEarliestDate.run();
-			
-			// Execute the third query after the second one
-			await dataDemandCurrent.run();
-			
-			await filteredDemandForecast.run();
-
-			// Continue with other actions or logic
+			await getProductForecast.run();
+			// await GetLatestDate.run();
+			// await GetEarliestDate.run();
+			// await dataDemandCurrent.run();
+			// await filteredDemandForecast.run();
 			console.log("All queries executed successfully");
 		} catch (error) {
 			console.error("Error executing queries:", error);
