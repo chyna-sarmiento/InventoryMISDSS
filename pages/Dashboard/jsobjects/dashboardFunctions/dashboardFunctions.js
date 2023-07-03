3f50640dc13788407c269b55bd40808d6703b86d
chyna-sarmiento <chyna-sarmiento>
2023-07-03 17:12:45 +0000

    fix (Dashboard): chart fix

--- pages/Dashboard/jsobjects/dashboardFunctions/dashboardFunctions.js
+++ pages/Dashboard/jsobjects/dashboardFunctions/dashboardFunctions.js
@@ -1,19 +1,17 @@
 export default {
 	async onTabSelected(tabName) {
-		// if(tabName == "Inventory Stock") {
-			// try {
-				// // Execute the third query after the second one
-				// await dataStockCurrent.run();
-// 
-				// await filteredStockForecast.run();
-// 
-				// // Continue with other actions or logic
-				// console.log("All queries executed successfully");
-			// } catch (error) {
-				// console.error("Error executing queries:", error);
-			// }
-		// } else {
-			// apiRunOrder.executeQueriesInOrder();
-		// }
+		if(tabName == "Item Demand") {
+			try {
+				await GetLatestDate.run();
+				await GetEarliestDate.run();
+				await dataDemandCurrent.run();
+				await filteredDemandForecast.run();
+
+				// Continue with other actions or logic
+				console.log("All queries executed successfully");
+			} catch (error) {
+				console.error("Error executing queries:", error);
+			}
+		}
 	}
 }
