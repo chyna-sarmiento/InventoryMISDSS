096b1ff1c6a5ddd06b8255fd877fbc30b53aaf16
chyna-sarmiento <chyna-sarmiento>
2023-07-09 07:34:40 +0000

    chore (Dashboard): add query order

--- pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
+++ pages/Dashboard/jsobjects/apiRunOrder/apiRunOrder.js
@@ -1,8 +1,14 @@
 export default {
 	async executeQueriesInOrder() {
 		try {
+			await GetLatestDate.run();
+			await ShowOutgoingPreview.run();
+			await GetDailyProfit.run();
+			await ShowProductList.run();
 			await getProductForecast.run();
-			// await GetLatestDate.run();
+			await GetNumberOfProducts.run();
+			await GetCountOutOfStock.run();
+			await GetCountLowStock.run();
 			// await GetEarliestDate.run();
 			// await dataDemandCurrent.run();
 			// await filteredDemandForecast.run();
